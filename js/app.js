fd9992aa4413eee05e4c3f277e07b71c6b2d13bb
Mykhailo Tiurin <mtyurin@gmail.com>
2015-08-25 00:54:08 +0200

    feat: Add collision handling

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var cellX = 101;
 var cellY = 83;
+var enemyOffsetY = 63; // serves to draw enemies aligned to the stones
 
 // Enemies our player must avoid
 var Enemy = function () {
@@ -14,14 +15,14 @@ var Enemy = function () {
 
 Enemy.prototype.init = function () {
   this.x = -cellX;
-  this.y = 63 + getRandomIntInclusive(0, 2) * cellY;
+  this.y = enemyOffsetY + getRandomIntInclusive(0, 2) * cellY;
   this.speed = getRandomIntInclusive(75, 375);
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function (dt) {
-  if (this.x > 505) {
+  if (this.x > 5 * cellX) {
     this.init();
   }
   var step = this.speed * dt;
@@ -45,7 +46,6 @@ function getRandomIntInclusive(min, max) {
 var Player = function () {
   this.sprite = 'images/char-boy.png';
   this.init();
-
 }
 
 Player.prototype.init = function () {
@@ -53,7 +53,17 @@ Player.prototype.init = function () {
   this.y = getRandomIntInclusive(4, 5) * cellY;
 }
 
-Player.prototype.update = function () {}
+Player.prototype.update = function (enemies) {
+  var that = this;
+  enemies.forEach(function (enemy) {
+    var distanceX = Math.abs(that.x - enemy.x);
+    var distanceY = Math.abs(that.y - enemy.y);
+
+    if (distanceX < cellX && distanceY < enemyOffsetY) {
+      that.init();
+    }
+  });
+}
 
 Player.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -62,7 +72,6 @@ Player.prototype.render = function () {
 Player.prototype.handleInput = function (direction) {
   if (direction === 'left' && this.x > 0) {
     this.x -= cellX;
-
   } else if (direction === 'right' && this.x < 4 * cellX) {
     this.x += cellX;
   } else if (direction === 'up' && this.y > 0) {
@@ -70,17 +79,13 @@ Player.prototype.handleInput = function (direction) {
   } else if (direction === 'down' && this.y < 5 * cellY) {
     this.y += cellY;
   }
-
 }
-
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 // Place the player object in a variable called player
 var player = new Player();
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
